a7686c5f3a3006c222e9b56680e830012f2f0111
Rowan Mulder <102707@student.horizoncollege.nl>
2020-11-04 07:47:38 +0100

    Added a healthbar

---
+++ Assets/Scripts/PlayerHealth/PlayerHealth.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealthpoints = 100;
+    public int currentHealthpoints = 100;
+    public Text TextHealthpoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TextHealthpoints.text = $"{currentHealthpoints}/{maxHealthpoints}";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Damage(int damage)
+    {
+        if ((currentHealthpoints -= damage) <= 0)
+        {
+            currentHealthpoints = 0;
+            Death();
+        }
+
+        TextHealthpoints.text = $"{currentHealthpoints}/{maxHealthpoints}";
+    }
+
+    void Death()
+    {
+        // Wait 5 seconds before reset to last checkpoint
+        // Display some UI?
+        // Blur/darken the screen?
+
+
+    }
+}
