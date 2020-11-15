eb4b991a87e19245c14eba5224598f86feec6d6d
Rowan Mulder <102707@student.horizoncollege.nl>
2020-11-15 20:30:57 +0100

    LateUpdate geïntroduceerd, onderscheid tussen aiming en niet-aiming voor animaties
    LateUpdate wordt na Update uitgevoerd. Hierdoor met lage prioriteit op LateUpdate kunnen animaties worden toegepast bovenop huidige animaties, wat gebruikt wordt om richten van wapens te visualiseren.
    
    Een aantal botten moeten stil blijven staan tijdens het richten, vanaf de ruggengraat (root) tot aan de schouder. (chest-animaties worden nog niet toegepast, dit kan later ook stilgezet worden in LateUpdate)

--- Assets/Scripts/PlayerLook/PlayerLook.cs
+++ Assets/Scripts/PlayerLook/PlayerLook.cs
@@ -11,11 +11,16 @@ public class PlayerLook : MonoBehaviour
     public Transform playerFirstPersonCamera;
     public Transform playerThirdPersonCamera;
     public Transform playerNeckbone;
+    public Transform playerSpineGlobal;
+    public Transform playerShoulderGlobal;
     public Transform playerUpperArmGlobal;
     public Transform playerUpperArmLocal;
     private Quaternion playerUpperArmGlobalRotation;
+    private Quaternion playerShoulderGlobalRotation;
+    private Quaternion playerSpineGlobalRotation;
     float xRotation = 0f;
     float yRotation = 0f;
+    bool aimingWeapon = true;
 
     // Start is called before the first frame update
     void Start()
@@ -24,12 +29,45 @@ public class PlayerLook : MonoBehaviour
         Cursor.lockState = CursorLockMode.Locked;
 
         playerUpperArmGlobalRotation = playerUpperArmGlobal.localRotation;
+        playerShoulderGlobalRotation = playerShoulderGlobal.localRotation;
+        playerSpineGlobalRotation = playerSpineGlobal.localRotation;
     }
 
     // Update is called once per frame
     void Update()
     {
         ApplyRotations();
+    }
+
+    // LateUpdate is called once per frame with lower priority than Update
+    void LateUpdate()
+    {
+        // Applies the xRotation to the player's neckbone.
+        playerNeckbone.localRotation = Quaternion.Euler(0f, 0f, xRotation);
+
+        // Applies the yRotation to the entire player.
+        transform.localRotation = Quaternion.Euler(0f, yRotation, 0f);
+
+        if (aimingWeapon)
+        {
+            //*/ Pistol aiming
+                // Applies the xRotation to the player's global lower arm.
+                playerUpperArmGlobal.localRotation = playerUpperArmGlobalRotation * Quaternion.Euler(0f + xRotation, 0f, 0f);
+
+                // Stops animating specific bodyparts for a more steady aim.
+                playerShoulderGlobal.localRotation = playerShoulderGlobalRotation;
+                playerSpineGlobal.localRotation = playerSpineGlobalRotation;
+            //*/
+
+            /*/ RPG aiming (example)
+                // Applies the xRotation to the player's spine.
+                playerSpineGlobal.localRotation = playerSpineGlobalRotation * *Quaternion.Euler(0f, 0f + xRotation, 0f);
+                
+                //Stops animating specific bodyparts for a more steady aim.
+                playerUpperArmGlobal.localRotation = playerUpperArmGlobalRotation;
+                playerShoulderGlobal.localRotation = playerShoulderGlobalRotation;
+            //*/
+        }
     }
 
     void ApplyRotations()
@@ -65,17 +103,5 @@ public class PlayerLook : MonoBehaviour
 
         // Applies the xRotation to the player's third-person camera.
         playerThirdPersonCamera.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-
-        // Applies the xRotation to the player's neckbone.
-        playerNeckbone.localRotation = Quaternion.Euler(0f, 0f, xRotation);
-
-        // Applies the xRotation to the player's global lower arm.
-        playerUpperArmGlobal.localRotation = playerUpperArmGlobalRotation * Quaternion.Euler(0f + xRotation, 0f, 0f);
-
-        // Applies the xRotation to the player's local lower arm.
-        //playerUpperArmLocal.localRotation = Quaternion.Euler(0f + xRotation, 0f, 0f);
-
-        // Applies the yRotation to the player object.
-        transform.localRotation = Quaternion.Euler(0f, yRotation, 0f);
     }
 }
