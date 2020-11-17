3dfe3e2d1628a712e6b576ad198146e87ee6db5d
Rowan Mulder <102707@student.horizoncollege.nl>
2020-11-17 21:12:54 +0100

    Stuurt de NavMeshAgent aan
    Dit zal nog worden uitgebreid. Dit is een eerste opzetje.

---
+++ Assets/Scripts/EnemyMove/EnemyMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMove : MonoBehaviour
+{
+    /// <summary>
+    /// Vaak voorkomen problemen bij NavMesh AI:
+    ///     "SetDestination" can only be called on an active agent that has been placed on a NavMesh.
+    ///         Bij gebruik van custom Agent profielnamen (buiten default "Humanoid") moet je schijnbaar NavMeshSurface gebruiken. Makkelijker is om de standaard "Humanoid" Agent te gebruiken.
+    ///         
+    /// </summary>
+
+    public Transform moveDirection;
+    public NavMeshAgent agent;
+    public bool movingToPlayer = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (movingToPlayer)
+        {
+            agent.SetDestination(moveDirection.position);
+        }
+    }
+}
