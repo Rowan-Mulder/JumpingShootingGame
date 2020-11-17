90ecc70e9f9b9fa050676ac9516bb179f51ae74a
Rowan Mulder <102707@student.horizoncollege.nl>
2020-11-17 21:02:22 +0100

    TargetHealth vervangen door EnemyHealth

---
+++ Assets/Scripts/EnemyHealth/EnemyHealth.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int maxHealthpoints = 100;
+    public int currentHealthpoints = 100;
+    public bool dead = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void UpdateHealth()
+    {
+        // May not be needed (yet)
+    }
+
+    // Enemy gets shot
+    public void Damage(int damage)
+    {
+        if ((currentHealthpoints -= damage) <= 0)
+        {
+            currentHealthpoints = 0;
+            Death();
+        }
+
+        UpdateHealth();
+    }
+
+    void Death()
+    {
+        // Activate death animation and destroy current object after some time for performance?
+
+        dead = true;
+    }
+
+    void Revive()
+    {
+        dead = false;
+        currentHealthpoints = maxHealthpoints;
+        UpdateHealth();
+    }
+}
