e7998c50f44843934673d623b5b316d3695c8c1b
Rowan Mulder <102707@student.horizoncollege.nl>
2020-11-22 17:07:12 +0100

    WorldController toegevoegd
    De wereld zal grotendeels worden aangestuurd door WorldController.cs, waar enemy spawn waves, item pickups, etc. worden behandeld.

---
+++ Assets/Scripts/WorldController/WorldController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldController : MonoBehaviour
+{
+    public GameObject enemyInstanciable;
+    public int enemiesToSpawn = 10;
+    public int enemiesSpawnLimit = 6;
+    public int enemiesSpawnLimitPerSpawnLocation;
+    public float enemiesSpawnDelayInMiliseconds = 1000f;
+    public bool enemiesReadyToSpawn = false;
+    public Transform[] spawnLocations;
+    public int spawnLocationsAmount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnLocationsAmount = spawnLocations.Length;
+        enemiesSpawnLimitPerSpawnLocation = Mathf.FloorToInt(enemiesSpawnLimit / spawnLocationsAmount);
+        enemiesReadyToSpawn = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enemiesReadyToSpawn && enemiesToSpawn > 0)
+        {
+            SpawnEnemy();
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        enemiesReadyToSpawn = false;
+
+        int randomNumber = RandomNumber(0, spawnLocationsAmount);
+        if (spawnLocations[randomNumber].childCount < enemiesSpawnLimitPerSpawnLocation) {
+            GameObject enemy = Instantiate(enemyInstanciable, spawnLocations[randomNumber]);
+            enemy.SetActive(true);
+
+            enemiesToSpawn--;
+        }
+
+        Invoke("PrepareNextEnemySpawn", (enemiesSpawnDelayInMiliseconds / 1000));
+    }
+
+    void PrepareNextEnemySpawn()
+    {
+        enemiesReadyToSpawn = true;
+    }
+
+    int RandomNumber(int min, int max)
+    {
+        return Random.Range(min, max);
+    }
+}
